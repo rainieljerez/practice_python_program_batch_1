c689c733b911108aaccb76c0260e8186aeaa4b30
rainieljerez <rainiel.jerez@gmail.com>
2026-03-12 16:41:36 +0800

    first batch of programs

--- sum_of_ten_numbers.py
+++ sum_of_ten_numbers.py
@@ -1,2 +1,11 @@
 #Prog07: Create a program that ask user to input 10 numbers.
 #Print the sum of all the numbers.
+
+#user is asked to input 10 numbers
+number = float(input("Enter a Number: "))
+
+#number of repetition
+for i in range (9):
+    number += float(input("Enter a Number: "))
+
+print (str(number) + " is the total of the ten numbers.")
